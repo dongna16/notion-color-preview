4f2c1fd3b7762edf2ebb1af21658621b9b5233b8
dongna <dongna16@icloud.com>
2025-04-13 00:51:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -36,21 +36,19 @@ const server = createServer((req, res) => {
   const checkerAlpha = (1 - alpha).toFixed(2)
 
   const svg = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 200 200" preserveAspectRatio="xMidYMid meet">
-    <g transform="translate(76,76)">
-      <rect width="48" height="48" fill="#${hex}" />
-      ${(() => {
-        let pattern = ""
-        for (let y = 0; y < 48; y += 8) {
-          for (let x = 24; x < 48; x += 8) {
-            const isDark = (x + y) % 16 === 0
-            const base = isDark ? "225,225,225" : "255,255,255"
-            pattern += `<rect x="${x}" y="${y}" width="8" height="8" fill="rgba(${base}, ${checkerAlpha})" />\n`
-          }
+  <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" shape-rendering="crispEdges">
+    <rect width="${width}" height="${height}" fill="#${hex}" />
+    ${(() => {
+      let pattern = ""
+      for (let y = 0; y < height; y += 8) {
+        for (let x = 24; x < width; x += 8) {
+          const isDark = (x + y) % 16 === 0
+          const base = isDark ? "225,225,225" : "255,255,255"
+          pattern += `<rect x="${x}" y="${y}" width="8" height="8" fill="rgba(${base}, ${checkerAlpha})" />\n`
         }
-        return pattern
-      })()}
-    </g>
+      }
+      return pattern
+    })()}
   </svg>
   `
 
